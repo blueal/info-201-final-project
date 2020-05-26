a5fc95cbe1b82af5ad8cfd10f24df23f131e58e9
Ryan Carroll <rcarro@uw.edu>
2020-05-26 15:59:24 -0700

    Add functions and style files

--- scripts/chart_1.R
+++ scripts/chart_1.R
@@ -10,7 +10,27 @@ library(plotly)
 library(stringr)
 library(vroom)
 
-seattle_data <- 
+
+most_pop_crime <- function(df_https) {
+  data <- vroom(df_https)
+  types_of_crime <- data %>%
+    mutate(count = 1) %>%
+    group_by(offense_parent_group) %>%
+    summarise(total_by_type = sum(count))
+
+  crime_frequency <- ggplot(types_of_crime) +
+    geom_col(mapping = aes(x = offense_parent_group, y = total_by_type)) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+    ggtitle("Which Crime Type is Most Popular?") +
+    theme(legend.position = "none") +
+    ylab("Number of Occurrances") +
+    xlab("Type of Crime") +
+    theme(axis.title.x = element_text(margin = margin(t = 20)))
+
+  return(crime_frequency)
+}
+seattle_data <-
   vroom("https://data.seattle.gov/resource/tazs-3rd5.csv")
 
 
@@ -18,12 +38,9 @@ seattle_data <-
 #take a look at what types of crimes there are in the data set
 unique(seattle_data$offense_parent_group)
 
-#choose a random part of the data set large enough to represent entire sample
-#data <- sample_n(seattle_data, size=1500)
-
 #check out how many of each crime was commited throught the years
-types_of_crime <- seattle_data %>% 
-  mutate(count = 1) %>% 
+types_of_crime <- seattle_data %>%
+  mutate(count = 1) %>%
   group_by(offense_parent_group) %>%
   summarise(total_by_type = sum(count))
 
@@ -41,3 +58,5 @@ crime_frequency <- ggplot(types_of_crime) +
   theme(axis.title.x = element_text(margin = margin(t = 20)))
 
 crime_frequency
+
+most_pop_crime("https://data.seattle.gov/resource/tazs-3rd5.csv")
