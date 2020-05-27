2a0420ed2e4711223f5aae55eb9c8de3c18d26f1
Alex Friedberg <blueal@users.noreply.github.com>
2020-05-27 00:04:39 -0700

    Cleaned up, deleted commented code in table.r

--- scripts/table.R
+++ scripts/table.R
@@ -39,24 +39,3 @@ avg_report_delay <- function(table) {
     pull()
   return(info)
 }
-
-
-# relevant info by neighborhood & offense
-
-# max_n <- neighborhood_info %>%
-#   filter(report_delay == max(report_delay, na.rm = TRUE))
-# min_n <- neighborhood_info %>%
-#   filter(report_delay == min(report_delay, na.rm = TRUE))
-# max_time_n <- round(pull(max_n, report_delay), digits = 2) # in days
-# min_time_n <- round(pull(min_n, report_delay), digits = 2) # in days
-# max_neighborhood <- pull(max_n, `MCPP`) # area with largest delay
-# min_neighborhood <- pull(min_n, `MCPP`) # area with smallest delay
-
-# max_o <- offense_info %>%
-#   filter(report_delay == max(report_delay, na.rm = TRUE))
-# min_o <- offense_info %>%
-#   filter(report_delay == min(report_delay, na.rm = TRUE))
-# max_time_o <- round(pull(max_o, report_delay), digits = 2) # in days
-# min_time_o <- round(pull(min_o, report_delay), digits = 2) # in days
-# max_offense <- pull(max_o, `Offense`) # offense with largest delay
-# min_offense <- pull(min_o, `Offense`) # offense with smallest delay
