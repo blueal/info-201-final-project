28bdfcfac80f8410d5541271a53496fbaefe8163
Jason Nguyen <jatn@uw.edu>
2020-05-31 19:46:24 -0700

    Update pages

--- deliverable/app_ui.R
+++ deliverable/app_ui.R
@@ -1,8 +1,14 @@
+source("overview.R")
 source("page_one.R")
+source("page_two.R")
+source("page_three.R")
+source("summary.R")
 ui <- fluidPage(
   h1("Seattle's Criminal Activity"),
   navbarPage(
     "Tabs",
-    map_page
+    overview,
+    map_page,
+    summary_info
   )
 )
