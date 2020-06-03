a989a5bd3419daa07c7b42220c1e3b718b602996
Kristina Chia-Chi Hsu <kristinahsu@gmail.com>
2020-06-04 02:12:09 +0800

    Edit layout and Add Description

--- deliverable/page_two.R
+++ deliverable/page_two.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(plotly)
 library(dplyr)
 library(vroom)
+
 seattle_data <- 
   vroom("https://data.seattle.gov/resource/tazs-3rd5.csv")
 
@@ -11,27 +12,45 @@ data_with_count <- seattle_data %>%
 
 names_of_filter <- c("Group" = "group_a_b",
                     "Category" = "crime_against_category",
-                    "Parent Group" = "offense_parent_group",
+                    "Offense Parent Group" = "offense_parent_group",
                     "Offense Type" = "offense")
 
 x_input <- selectInput(
   "x_var",
   inputId = "x_var",
-  label = "Variable",
+  label = "Count by",
   choices = names_of_filter
 )
+
 y_input <- selectInput(
   "y_group",
   inputId = "y_group",
-  label = "Group",
+  label = "Color by",
   choices = names_of_filter
 )
 
 crime_type_page <- tabPanel(
   "Criminal Activity Types and Frequency",
   p("In this section, we will be using the Seattle dataset
-  to group crimes in diffent ways and show the frequency of them."),
-  x_input,
-  y_input,
+  to group crimes in diffent ways and show the frequency of the grouping."),
+  sidebarPanel(
+    x_input,
+    y_input
+  ),
+  sidebarPanel(
+    width = 8,
+    h4("Description"),
+    p("For this chart, we can answer the question what crime type is the 
+    most popular. We decided to use a bar chart because it is the clearest 
+    way to display comparison between counts of data. Therefore, we can
+    easily trace each intersection and know that ", em("Larceny-Theft"),
+    " is the most popular parent crime group in the Seattle area. 
+    Also the color ledgend allow us to see two grouping at the same time.
+    For example, the parent crime group, ", em("Larceny-Theft"),
+    "'s most popular crime against catagory is of, ", em("PROPERTY"),
+    ".")
+  ),
   plotOutput("bar")
+  
 )
+
