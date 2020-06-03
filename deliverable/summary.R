64021b06fcadc6a97376796bf373a1a8f93ea53c
Alex Friedberg <blueal@users.noreply.github.com>
2020-06-03 01:33:06 -0700

    Added some boiler plate stuff to summary

--- deliverable/summary.R
+++ deliverable/summary.R
@@ -1,3 +1,13 @@
 summary_info <- tabPanel(
   "Summary"
 )
+
+main <- mainPanel(
+  h1("Summary of Analysis"),
+  h2("Finding #1"),
+  p("Answersr"),
+  h2("Finding #2"),
+  p("Stuf Stuff"),
+  h2("Finding 3"),
+  p("Lorem Ipsum is cool")
+)
