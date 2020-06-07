81142a5b3359813f3859bbf178748ac76c9ba935
Jason Nguyen <jatn@uw.edu>
2020-06-07 14:18:27 -0700

    Update map function; changes map plotting according to offense type

--- deliverable/page_one.R
+++ deliverable/page_one.R
@@ -33,8 +33,9 @@ description <- paste0(
 map_page <- tabPanel(
   "Mapping Criminal Activity",
   h2("Seattle Map of Reported Criminal Offense"),
-  p("In this section, the Seattle dataset will be 
-    used to map the locations of different incidents."),
+  p("In this section, the Seattle dataset will be used to map the locations
+    of different incidents. This first map gives a general overview of the
+    different criminal offenses."),
   sidebarLayout(
     mainPanel(
       leafletOutput(
@@ -45,6 +46,9 @@ map_page <- tabPanel(
       selectInput("colors", "Color Scheme",
         rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
       ),
+      selectInput("offense", "Offense",
+                  choices = c(location$offense)
+      ),
       selectInput("key", "Key", choices = names(location[1:2])
       ),
       checkboxInput(inputId = "legend", "Show Legend", TRUE
@@ -52,7 +56,9 @@ map_page <- tabPanel(
     )
   ),
   h2("Insights"),
-  p("This interactive map displays not only the relative location of
+  sidebarPanel(
+    width = 8,
+    p("This interactive map displays not only the relative location of
     each, but also the", strong("address"), ", ", strong("crime label"), 
     ", ", strong("crime type"), ", and ", strong("offense"), ". Looking
     at the crime type, the majority of the criminal incidents were offenses
@@ -60,7 +66,11 @@ map_page <- tabPanel(
     damage/destruction to 'PROPERTY' occur more frequently than those 
     categorized as damage/destruction to 'SOCIETY'. Likewise, each point
     on the map appears to group around First Hill and Belltown, which are
-    precincts W and E.")
+    precincts W and E. An interesting insight that we found was that
+    one of the least occurring offenses were", strong("wire fraud"), " and",
+    strong("embezzlement"), ", where there were no more than 3 incidents
+    for each offense.")
+  )
 )
 
   
