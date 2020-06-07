2e41e950d26312860edbe7fc829a96db7d55f65d
Anne Farley <afarley102@gmail.com>
2020-06-07 14:54:15 -0700

    should be the same

--- deliverable/page_one.R
+++ deliverable/page_one.R
@@ -1,4 +1,3 @@
-# Creates leaflet map for Seattle dataset
 seattle_data_small <- 
   vroom("https://data.seattle.gov/resource/tazs-3rd5.csv")
 
@@ -33,8 +32,9 @@ description <- paste0(
 map_page <- tabPanel(
   "Mapping Criminal Activity",
   h2("Seattle Map of Reported Criminal Offense"),
-  p("In this section, the Seattle dataset will be 
-    used to map the locations of different incidents."),
+  p("In this section, the Seattle dataset will be used to map the locations
+    of different incidents. This first map gives a general overview of the
+    different criminal offenses."),
   sidebarLayout(
     mainPanel(
       leafletOutput(
@@ -43,7 +43,10 @@ map_page <- tabPanel(
     ),
     sidebarPanel(
       selectInput("colors", "Color Scheme",
-        rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+                  rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+      ),
+      selectInput("offense", "Offense",
+                  choices = c(location$offense)
       ),
       selectInput("key", "Key", choices = names(location[1:2])
       ),
@@ -52,15 +55,19 @@ map_page <- tabPanel(
     )
   ),
   h2("Insights"),
-  p("This interactive map displays not only the relative location of
+  sidebarPanel(
+    width = 8,
+    p("This interactive map displays not only the relative location of
     each, but also the", strong("address"), ", ", strong("crime label"), 
-    ", ", strong("crime type"), ", and ", strong("offense"), ". Looking
+      ", ", strong("crime type"), ", and ", strong("offense"), ". Looking
     at the crime type, the majority of the criminal incidents were offenses
     to 'PROPERTY'. What this means is that incidents that are catgeorized as
     damage/destruction to 'PROPERTY' occur more frequently than those 
     categorized as damage/destruction to 'SOCIETY'. Likewise, each point
     on the map appears to group around First Hill and Belltown, which are
-    precincts W and E.")
+    precincts W and E. An interesting insight that we found was that
+    one of the least occurring offenses were", strong("wire fraud"), " and",
+      strong("embezzlement"), ", where there were no more than 3 incidents
+    for each offense.")
+  )
 )
-
-  
