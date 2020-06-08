49fa360ca010fcf1a9686cd6516ae97d895b5e60
Alex Friedberg <blueal@users.noreply.github.com>
2020-06-07 23:42:17 -0700

    #Commented the summary #More comments

--- deliverable/summary.R
+++ deliverable/summary.R
@@ -3,23 +3,27 @@
 # safestNeighborhoods, badestNeighborhoods, MostCommonCrimes, LeastCommonCrimes
 # all of these fields return a 5 value list of the top items
 
-
+#Create summary of information based on data
 summary_table <- get_summary_info(seattle_data1)
 
 
-
+#Create the data for the first table
+#Get the neighborhoods with the highest crime
 neighborhood_crime <- seattle_data1 %>%
   group_by(MCPP) %>%
   summarise(count = n()) %>%
   arrange(desc(count)) %>%
   head(10)
 
+#Create the data for the second table
+#Get the top 10 most commmon offenses
 types_of_crime <- seattle_data1 %>%
   group_by(Offense) %>%
   summarise(count = n()) %>%
   arrange(desc(count)) %>%
   head(10)
 
+#Let's create our text!
 crime_density <- mainPanel(
   h1("Summary of Analysis"),
   h2("Crime Density"),
@@ -30,6 +34,7 @@ crime_density <- mainPanel(
     had noticable amounts of crime reports, including that of
     Capitol Hill which is easily accesible to the Central Buisness District.
     "),
+  #Output our first plot
   plotOutput("neghborhood_bar_chart"),
   h2("Most Common Crimes"),
   p("When analyzing the different types of crime in our dataset we noticed that
@@ -41,6 +46,7 @@ crime_density <- mainPanel(
     between this crime, and the next one. We are able to conclude that this
     crime is of extraordinaty prevalence. Many neighborhoods report this as the
     most common crime."),
+  #Second plot goes here
   plotOutput("types_bar_chart"),
   h2("Timing of crimes"),
   p("Many people beleive lots of crimes occur at night but the data shows that
@@ -55,6 +61,7 @@ crime_density <- mainPanel(
     and Arson.")
 )
 
+#Generage Summary Page
 summary_info <- tabPanel(
   "Summary",
   crime_density
