36ace9d1b3b0b356db0f679433e0eeaeb6edeb49
Jason Nguyen <jatn@uw.edu>
2020-06-07 23:17:57 -0700

    Add insightful description and comments

--- deliverable/page_three.R
+++ deliverable/page_three.R
@@ -1,5 +1,7 @@
 source("datasets.R")
 
+# Creates a tab page consisting of a bar plot
+# and an insightful description
 time_page <- tabPanel(
   "Time of Criminal Activity",
   h2("Number of Crimes per Hour"),
@@ -16,6 +18,13 @@ time_page <- tabPanel(
   sidebarPanel(
     width = 8,
     p("In this section, the Seattle Dataset will be used to identify and show
-    what crimes occur at certain times of the day"),
+    what crimes occur at certain times of the day. An important insight that 
+    can be drawn from this plot is that for the majority of the offenses,
+    most of the offenses occurred at the 0 hour, or midnight. Offenses may
+    occur more at midnight than any other hour because it gets dark and 
+    many people are inactive at that time so it's less likely to be caught
+    in the act (except for those counted in this dataset). Likewise, animal
+    cruelty and gambling offenses had some of the least number of occurrences
+    with six and four total occurrences respectively."),
   )
 )
