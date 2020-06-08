7fcb43321b2b53fbf7d4cb570106c5fcc3a524e5
Anne Farley <afarley102@gmail.com>
2020-06-07 19:58:33 -0700

    slight changes

--- deliverable/page_three.R
+++ deliverable/page_three.R
@@ -4,17 +4,16 @@ time_page <- tabPanel(
   "Time of Criminal Activity",
   h2("Number of Crimes per Hour"),
   p("To begin select a crime of your choice in the drop down menu below."),
-  mainPanel(
-    plotOutput(outputId = "barchart")
+  sidebarLayout(
+    mainPanel(plotOutput("barchart")),
+    sidebarPanel(
+      selectInput(inputId = "type_of_crime",
+                label = "Type of Crime",
+                choices = c(get_hours$`Offense Parent Group`))
+    )
   ),
+  h2("Insights"),
   sidebarPanel(
-    selectInput(inputId = "type_of_crime",
-              label = "Type of Crime",
-              choices = c(get_hours$`Offense Parent Group`))
-  ),
-  tags$hr(),
-  sidebarPanel(
-    h2("Insights"),
     width = 8,
     p("In this section, the Seattle Dataset will be used to identify and show
     what crimes occur at certain times of the day"),
