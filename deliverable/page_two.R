af172fda409fe9c76d2a45b518fea231680eeb4f
Kristina Chia-Chi Hsu <kristinahsu@gmail.com>
2020-06-08 13:37:59 +0800

    Add comments

--- deliverable/page_two.R
+++ deliverable/page_two.R
@@ -1,9 +1,11 @@
 source("datasets.R")
 
+# create named-list of allows user choices
 names_of_filter <- c("Category" = "Crime Against Category",
                     "Offense Parent Group" = "Offense Parent Group",
                     "Offense Type" = "Offense")
 
+# Add a selectInput that allows choice of grouping
 x_input <- selectInput(
   "x_var",
   inputId = "x_var",
@@ -11,6 +13,7 @@ x_input <- selectInput(
   choices = names_of_filter
 )
 
+# Add a selectInput that allows choice of coloring
 y_input <- selectInput(
   "y_group",
   inputId = "y_group",
@@ -18,16 +21,19 @@ y_input <- selectInput(
   choices = names_of_filter
 )
 
+# Create tab
 crime_type_page <- tabPanel(
   "Frequency of Criminal Activity",
   h2("Seattle Data Crime Types Exploration"),
   p("To begin, choose which category occurances will be counted & which will
     be differentiated by color."),
+  # Format displayment
   sidebarLayout(
     mainPanel(
       plotOutput("bar"),
       h2("Insights"),
       sidebarPanel(
+        # edit sidebarPanel size to line up with the plot size
         width = 12,
         p("This chart can answer the question: what crime type is the
         most popular? Because a bar chart is the clearest way to display
